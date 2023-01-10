82676a1ac552273fddb5a4a32e2965734930352f
Zuber Ustad <72431298+Zuber8040@users.noreply.github.com>
2023-01-10 23:46:18 +0530

    added radom quote

--- data.js
+++ data.js
@@ -1,11 +1,83 @@
-const labels = Utils.months({count: 7});
-const data = {
-  labels: labels,
+
+// https://api.thingspeak.com/channels/1944585/field/1.json
+
+function updateChart(){
+  async function fetchData(){
+    const url = 'https://api.thingspeak.com/channels/1944585/field/1.json';
+    const response =await fetch(url);
+    // wait until the request has completed
+    const datapoints  = await response.json();
+    // console.log
+    // console.log(datapoints);  
+    return datapoints;
+  };
+
+    fetchData().then(datapoints=>{
+      let field12 = (datapoints.feeds.map(function(index){
+        return index.field1
+      }))
+      // console.log(field12)
+  
+      let created_at = (datapoints.feeds.map(function(index){
+        return index.created_at
+      }))
+      // console.log(created_at)
+
+      lineChart.config.data.labels=created_at;
+      lineChart.config.data.datasets[0].data=field12;
+      lineChart.update();
+
+
+  })
+}
+updateChart();
+
+
+
+// setup
+const data ={
+  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
   datasets: [{
-    label: 'My First Dataset',
-    data: [65, 59, 80, 81, 56, 55, 40],
-    fill: false,
-    borderColor: 'rgb(75, 192, 192)',
-    tension: 0.1
+    // date and time
+    label: '# of Votes',
+    // data 
+    data: [12, 19, 3, 5, 2, 3],
+    borderWidth: 1
   }]
 };
+
+
+// config
+
+const config = {
+  type: 'line',
+    data,
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      },
+      responsive:true
+    }
+}
+
+
+// render init block 
+
+const lineChart = new Chart(document.getElementById('lineChart'),config);
+
+// // 2nd lineChart
+// const lineChart2 = new Chart(document.getElementById('lineChart2'),config);
+
+// // // 3rd
+// const lineChart3 = new Chart(document.getElementById('lineChart3'),config);
+
+// // // 4th
+// const lineChart4 = new Chart(document.getElementById('lineChart4'),config);
+ 
+// // // 5th 
+// const lineChart5 = new Chart(document.getElementById('lineChart5'),config);
+
+// // // 6th
+// const lineChart6 = new Chart(document.getElementById('lineChart6'),config);
---
+++ script.js
@@ -0,0 +1,96 @@
+const slogans = [ "If u pollute the air, then the air will pollute u!",
+    "No Solution, more Air Pollution!",
+    "Give Me Clean Air or Give Me Death",
+    "Breath healthy and be healthy",
+    "Smokey? REPAIR!! Care for the air.",
+    "Kill Air Pollution",
+    "I Deserve the Blue",
+    "Don’t Besmear the Atmosphere",
+    "The Simple Life Causes Less Strife.",
+    "Let’s clear the air",
+    "You are what you breathe",
+    "Don’t be a brute! Don’t pollute!",
+    "Remove Pollution, increase the life",
+    "Don’t Be Mean keep your planet clean",
+    "Stop factories and industries, and gain more rain.",
+    "Clean Air for a better future",
+    "Let’s Move Towards Green",
+    "Rise the voice Agains pollution",
+    "Care for air.",
+    "Save the critters, don’t litter!",
+    "Stop air pollution before it stops our breathing.",
+    "Save your breath to feel free",
+    "Stop pollution by mitigation.",
+    "Because you deserve to breathe clean",
+    "Don’t be part of air pollution",
+    "Air Pollution is a silent enemy",
+    "Show care about the air.",
+    "Because you deserve to feel secure",
+    "Take a deep breath, don’t choke.",
+    "Global warming is real. Don’t pollute it.",
+    "Air Pollution isn’t cool so don’t be a fool.",
+    "Less Air pollution is the best solution",
+    "Join the revolution, stop the Air pollution!",
+    "When They Run Out, You’ll Be Without.",
+    "Whoever pollutes is our common enemy.",
+    "Wipe Out Pollution Before It Wipes You Out.",
+    "YOU are the key to cleaner Air.",
+    "Take Care Of The Trees, They Will Take Care Of You.",
+    "Air Pollution – The Silent Killer",
+    "Be nice to the air, we have to share",
+    "Air Pollution is not a joke. Air Pollution will make you choke",
+    "Pure Air, Pure life",
+    "Do care for the air",
+    "Polluting is not fair.",
+    "Air Pollution is a slow poison",
+    "Say no to air pollution",
+    "Don’t cut trees is a risk",
+    "Let the greenery rise",
+    "Be safe, Be aware",
+    "Don’t you dare to pollute the air?"
+]
+     
+
+
+setInterval(function(){ 
+    // number = Math.floor((Math.random()*100)+1);
+    // //other code
+    // console.log(number)
+
+    let min =1,max=50;
+    // find diff
+    let difference = max - min;
+
+    // generate random number 
+    let rand = Math.random();
+
+    // multiply with difference 
+    rand = Math.floor( rand * difference);
+
+    // add with min value 
+    rand = rand + min;
+
+    let heading=document.getElementById('heading');
+    heading.innerHTML=slogans[rand]
+
+
+
+
+}, 1000);
+
+
+function generateRandom() {
+   
+}
+
+console.log(generateRandom());
+
+// function fetchData(){
+    
+//     let heading=document.getElementById('heading');
+//     let ans
+//     heading.innerHTML=slogans[ans];
+// }
+// console.log(setTimeout(fetchData, 2000));
+
+// clearInterval(interval); // The interval will no longer run
