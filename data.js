692959549e3586ae5dc74248a7340c530655c13b
Zuber Ustad <72431298+Zuber8040@users.noreply.github.com>
2023-04-17 10:13:35 +0530

    removed charts

--- data.js
+++ data.js
@@ -1,282 +1,283 @@
 // https://api.thingspeak.com/channels/1944585/field/1.json
 
-const dataArray = [];
-const dataArray2 = [];
-const dataArray3 = [];
-const dataArray4 = [];
-const dataArray5 = [];
-const dataArray6 = [];	
+// const dataArray = [];
+// const dataArray2 = [];
+// const dataArray3 = [];
+// const dataArray4 = [];
+// const dataArray5 = [];
+// const dataArray6 = [];	
 
-$.getJSON(
-  "https://api.thingspeak.com/channels/1944585/field/1.json",
-  function (data) {
-    $.each(data, function (key, val) {
-      for (let i = 0; i < val.length; i++) {
-        let date_string = Date.parse(val[i].created_at);
-        dataArray.push({ x: date_string, y: val[i].field1 });
-      }
-    });
-    // console.log(dataArray);
-    chart.updateSeries([
-      {
-        name: "MQ 2 Data",
-        data: dataArray,
-      },
-    ]);
-  }
-);
+// $.getJSON(
+//   "https://api.thingspeak.com/channels/1944585/field/1.json",
+//   function (data) {
+//     $.each(data, function (key, val) {
+//       for (let i = 0; i < val.length; i++) {
+//         let date_string = Date.parse(val[i].created_at);
+//         dataArray.push({ x: date_string, y: val[i].field1 });
+//       }
+//     });
+//     // console.log(dataArray);
+//     chart.updateSeries([
+//       {
+//         name: "MQ 2 Data",
+//         data: dataArray,
+//       },
+//     ]);
+//   }
+// );
 
-var options = {
-  chart: {
-    type: "area",
-    height: 350,
-    zoom: {
-      autoScaleYaxis: true,
-    },
-  },
-  dataLabels: {
-    enabled: false,
-  },
-  series: [],
-  title: {
-    text: "MQ 2 Data",
-  },
-  noData: {
-    text: "Loading...",
-  },
-  xaxis: {
-    type: "datetime",
-  },
-};
+// var options = {
+//   chart: {
+//     type: "area",
+//     height: 350,
+//     zoom: {
+//       autoScaleYaxis: true,
+//     },
+//   },
+//   dataLabels: {
+//     enabled: false,
+//   },
+//   series: [],
+//   title: {
+//     text: "MQ 2 Data",
+//   },
+//   noData: {
+//     text: "Loading...",
+//   },
+//   xaxis: {
+//     type: "datetime",
+//   },
+// };
 
-var chart = new ApexCharts(document.querySelector("#chart"), options);
+// var chart = new ApexCharts(document.querySelector("#chart"), options);
 
-chart.render();
+// chart.render();
 
-$.getJSON(
-  "https://api.thingspeak.com/channels/1944585/field/2.json",
-  function (data) {
-    $.each(data, function (key, val) {
-      for (let i = 0; i < val.length; i++) {
-        let date_string = Date.parse(val[i].created_at);
-        dataArray2.push({ x: date_string, y: val[i].field2 });
-      }
-    });
-    // console.log(dataArray2);
-    chart2.updateSeries([
-      {
-        name: "MQ 9 Data",
-        data: dataArray2,
-      },
-    ]);
-  }
-);
+// $.getJSON(
+//   "https://api.thingspeak.com/channels/1944585/field/2.json",
+//   function (data) {
+//     $.each(data, function (key, val) {
+//       for (let i = 0; i < val.length; i++) {
+//         let date_string = Date.parse(val[i].created_at);
+//         dataArray2.push({ x: date_string, y: val[i].field2 });
+//       }
+//     });
+//     // console.log(dataArray2);
+//     chart2.updateSeries([
+//       {
+//         name: "MQ 9 Data",
+//         data: dataArray2,
+//       },
+//     ]);
+//   }
+// );
 
-var options2 = {
-  chart: {
-    type: "area",
-    height: 350,
-    zoom: {
-      autoScaleYaxis: true,
-    },
-  },
-  dataLabels: {
-    enabled: false,
-  },
-  series: [],
-  title: {
-    text: "MQ 9 Data",
-  },
-  noData: {
-    text: "Loading...",
-  },
-  xaxis: {
-    type: "datetime",
-  },
-};
+// var options2 = {
+//   chart: {
+//     type: "area",
+//     height: 350,
+//     zoom: {
+//       autoScaleYaxis: true,
+//     },
+//   },
+//   dataLabels: {
+//     enabled: false,
+//   },
+//   series: [],
+//   title: {
+//     text: "MQ 9 Data",
+//   },
+//   noData: {
+//     text: "Loading...",
+//   },
+//   xaxis: {
+//     type: "datetime",
+//   },
+// };
 
-var chart2 = new ApexCharts(document.querySelector("#chart2"), options2);
-chart2.render();
+// var chart2 = new ApexCharts(document.querySelector("#chart2"), options2);
+// chart2.render();
 
-$.getJSON(
-  "https://api.thingspeak.com/channels/1944585/field/3.json",
-  function (data) {
-    $.each(data, function (key, val) {
-      for (let i = 0; i < val.length; i++) {
-        let date_string = Date.parse(val[i].created_at);
-        dataArray3.push({ x: date_string, y: val[i].field3 });
-      }
-    });
-    // console.log(dataArray3);
-    chart3.updateSeries([
-      {
-        name: "MQ 135 Data",
-        data: dataArray3,
-      },
-    ]);
-  }
-);
+// $.getJSON(
+//   "https://api.thingspeak.com/channels/1944585/field/3.json",
+//   function (data) {
+//     $.each(data, function (key, val) {
+//       for (let i = 0; i < val.length; i++) {
+//         let date_string = Date.parse(val[i].created_at);
+//         dataArray3.push({ x: date_string, y: val[i].field3 });
+//       }
+//     });
+//     // console.log(dataArray3);
+//     chart3.updateSeries([
+//       {
+//         name: "MQ 135 Data",
+//         data: dataArray3,
+//       },
+//     ]);
+//   }
+// );
 
-var options3 = {
-  chart: {
-    type: "area",
-    height: 350,
-    zoom: {
-      autoScaleYaxis: true,
-    },
-  },
-  dataLabels: {
-    enabled: false,
-  },
-  series: [],
-  title: {
-    text: "MQ 135 Data",
-  },
-  noData: {
-    text: "Loading...",
-  },
-  xaxis: {
-    type: "datetime",
-  },
-};
+// var options3 = {
+//   chart: {
+//     type: "area",
+//     height: 350,
+//     zoom: {
+//       autoScaleYaxis: true,
+//     },
+//   },
+//   dataLabels: {
+//     enabled: false,
+//   },
+//   series: [],
+//   title: {
+//     text: "MQ 135 Data",
+//   },
+//   noData: {
+//     text: "Loading...",
+//   },
+//   xaxis: {
+//     type: "datetime",
+//   },
+// };
 
-var chart3 = new ApexCharts(document.querySelector("#chart3"), options3);
-chart3.render();
+// var chart3 = new ApexCharts(document.querySelector("#chart3"), options3);
+// chart3.render();
 
-// chart4
+// // chart4
 
-$.getJSON(
-  "https://api.thingspeak.com/channels/1944585/field/4.json",
-  function (data) {
-    $.each(data, function (key, val) {
-      for (let i = 0; i < val.length; i++) {
-        let date_string = Date.parse(val[i].created_at);
-        dataArray4.push({ x: date_string, y: val[i].field4 });
-      }
-    });
-    // console.log(dataArray4);
-    chart4.updateSeries([
-      {
-        name: "MQ 136 Data",
-        data: dataArray4,
-      },
-    ]);
-  }
-);
-var options4 = {
-  chart: {
-    type: "area",
-    height: 350,
-    zoom: {
-      autoScaleYaxis: true,
-    },
-  },
-  dataLabels: {
-    enabled: false,
-  },
-  series: [],
-  title: {
-    text: "MQ 136 Data",
-  },
-  noData: {
-    text: "Loading...",
-  },
-  xaxis: {
-    type: "datetime",
-  },
-};
-var chart4 = new ApexCharts(document.querySelector("#chart4"), options4);
-chart4.render();
+// $.getJSON(
+//   "https://api.thingspeak.com/channels/1944585/field/4.json",
+//   function (data) {
+//     $.each(data, function (key, val) {
+//       for (let i = 0; i < val.length; i++) {
+//         let date_string = Date.parse(val[i].created_at);
+//         dataArray4.push({ x: date_string, y: val[i].field4 });
+//       }
+//     });
+//     // console.log(dataArray4);
+//     chart4.updateSeries([
+//       {
+//         name: "MQ 136 Data",
+//         data: dataArray4,
+//       },
+//     ]);
+//   }
+// );
+// var options4 = {
+//   chart: {
+//     type: "area",
+//     height: 350,
+//     zoom: {
+//       autoScaleYaxis: true,
+//     },
+//   },
+//   dataLabels: {
+//     enabled: false,
+//   },
+//   series: [],
+//   title: {
+//     text: "MQ 136 Data",
+//   },
+//   noData: {
+//     text: "Loading...",
+//   },
+//   xaxis: {
+//     type: "datetime",
+//   },
+// };
+// var chart4 = new ApexCharts(document.querySelector("#chart4"), options4);
+// chart4.render();
 
-// chart  5
+// // chart  5
 
-$.getJSON(
-  "https://api.thingspeak.com/channels/1944585/field/5.json",
-  function (data) {
-    $.each(data, function (key, val) {
-      for (let i = 0; i < val.length; i++) {
-        let date_string = Date.parse(val[i].created_at);
-        dataArray5.push({ x: date_string, y: val[i].field5 });
-      }
-    });
-    // console.log(dataArray5);
-    chart5.updateSeries([
-      {
-        name: "Temperature",
-        data: dataArray4,
-      },
-    ]);
-  }
-);
-var options5 = {
-  chart: {
-    type: "area",
-    height: 350,
-    zoom: {
-      autoScaleYaxis: true,
-    },
-  },
-  dataLabels: {
-    enabled: false,
-  },
-  series: [],
-  title: {
-    text: "Temperature",
-  },
-  noData: {
-    text: "Loading...",
-  },
-  xaxis: {
-    type: "datetime",
-  },
-};
-var chart5 = new ApexCharts(document.querySelector("#chart5"), options5);
-chart5.render();
+// $.getJSON(
+//   "https://api.thingspeak.com/channels/1944585/field/5.json",
+//   function (data) {
+//     $.each(data, function (key, val) {
+//       for (let i = 0; i < val.length; i++) {
+//         let date_string = Date.parse(val[i].created_at);
+//         dataArray5.push({ x: date_string, y: val[i].field5 });
+//       }
+//     });
+//     // console.log(dataArray5);
+//     chart5.updateSeries([
+//       {
+//         name: "Temperature",
+//         data: dataArray4,
+//       },
+//     ]);
+//   }
+// );
+// var options5 = {
+//   chart: {
+//     type: "area",
+//     height: 350,
+//     zoom: {
+//       autoScaleYaxis: true,
+//     },
+//   },
+//   dataLabels: {
+//     enabled: false,
+//   },
+//   series: [],
+//   title: {
+//     text: "Temperature",
+//   },
+//   noData: {
+//     text: "Loading...",
+//   },
+//   xaxis: {
+//     type: "datetime",
+//   },
+// };
+// var chart5 = new ApexCharts(document.querySelector("#chart5"), options5);
+// chart5.render();
 
 
-$.getJSON(
-  "https://api.thingspeak.com/channels/1944585/field/6.json",
-  function (data) {
-    $.each(data, function (key, val) {
-      for (let i = 0; i < val.length; i++) {
-        let date_string = Date.parse(val[i].created_at);
-        dataArray6.push({ x: date_string, y: val[i].field6 });
-      }
-    });
-    // console.log(dataArray6);
-    chart6.updateSeries([
-      {
-        name: "Humidity",
-        data: dataArray6,
-      },
-    ]);
-  }
-);
-var options6 = {
-  chart: {
-    type: "area",
-    height: 350,
-    zoom: {
-      autoScaleYaxis: true,
-    },
-  },
-  dataLabels: {
-    enabled: false,
-  },
-  series: [],
-  title: {
-    text: "Humidity",
-  },
-  noData: {
-    text: "Loading...",
-  },
-  xaxis: {
-    type: "datetime",
-  },
-};
-var chart6 = new ApexCharts(document.querySelector("#chart6"), options6);
-chart6.render();
+// $.getJSON(
+//   "https://api.thingspeak.com/channels/1944585/field/6.json",
+//   function (data) {
+//     $.each(data, function (key, val) {
+//       for (let i = 0; i < val.length; i++) {
+//         let date_string = Date.parse(val[i].created_at);
+//         dataArray6.push({ x: date_string, y: val[i].field6 });
+//       }
+//     });
+//     // console.log(dataArray6);
+//     chart6.updateSeries([
+//       {
+//         name: "Humidity",
+//         data: dataArray6,
+//       },
+//     ]);
+//   }
+// );
+// var options6 = {
+//   chart: {
+//     type: "area",
+//     height: 350,
+//     zoom: {
+//       autoScaleYaxis: true,
+//     },
+//   },
+//   dataLabels: {
+//     enabled: false,
+//   },
+//   series: [],
+//   title: {
+//     text: "Humidity",
+//   },
+//   noData: {
+//     text: "Loading...",
+//   },
+//   xaxis: {
+//     type: "datetime",
+//   },
+// };
+// var chart6 = new ApexCharts(document.querySelector("#chart6"), options6);
+// chart6.render();
 
+const avgvalue1 =[];
 
 function updatedata1(){
 	async function fetchData(){
@@ -291,6 +292,9 @@ function updatedata1(){
 		  	// console.log(index.field1)
 			let mq2val = document.querySelector(".sensormq2value");
 			mq2val.innerHTML = index.field1;
+       avgvalue1.push(index.field1);
+      //  console.log(avgvalue1)
+       
 		}))
 		let created_at = (datapoints.feeds.map(function(index){
 			const isotime = index.created_at;
@@ -298,7 +302,6 @@ function updatedata1(){
 			
 			const sensormq2time = document.querySelector(".sensormq2time");
 			sensormq2time.innerHTML = localtime;
-		//   return index.created_at
 		}))
 	
 		
@@ -321,13 +324,13 @@ function updatedata2(){
 		let field12 = (datapoints.feeds.map(function(index){
 		  	// console.log(index.field1)
 			let sensormq9value = document.querySelector(".sensormq9value");
-			sensormq9value.textContent = index.field2;
-			console.log(index.field1);
+			sensormq9value.textContent = index.field2-3;
+			// console.log(index.field1);
 		}))
 		let created_at = (datapoints.feeds.map(function(index){
 			const isotime = index.created_at;
 			const localtime = new Date(isotime).toLocaleString();
-			console.log(localtime)
+			// console.log(localtime)
 			const sensormq9time = document.querySelector(".sensormq9time");
 			sensormq9time.innerHTML = localtime;
 			
@@ -357,13 +360,13 @@ function updatedata3(){
 		let field12 = (datapoints.feeds.map(function(index){
 		  	// console.log(index.field1)
 			let sensormq135value = document.querySelector(".sensormq135value");
-			sensormq135value.textContent = index.field3;
-			console.log(index.field3);
+			sensormq135value.textContent = index.field3-4;
+			// console.log(index.field3);
 		}))
 		let created_at = (datapoints.feeds.map(function(index){
 			const isotime = index.created_at;
 			const localtime = new Date(isotime).toLocaleString();
-			console.log(localtime)
+			// console.log(localtime)
 			const sensormq135time = document.querySelector(".sensormq135time");
 			sensormq135time.innerHTML = localtime;
 		//   return index.created_at
@@ -391,7 +394,7 @@ function updatedata4(){
 		let field12 = (datapoints.feeds.map(function(index){
 		  	// console.log(index.field1)
 			let sensormq136value = document.querySelector(".sensormq136value");
-			sensormq136value.textContent = index.field4;
+			sensormq136value.textContent = index.field4-10;
 			
 		}))
 		let created_at = (datapoints.feeds.map(function(index){
@@ -463,3 +466,51 @@ function updatedata6(){
 	})
   }
 updatedata6();
+
+// Initialize the global variable
+let lastValues1=[];
+let lastValues2 = [];
+let lastValues3 = [];
+let lastValues4 = [];
+
+  
+// Define the API endpoints
+const urls = [
+  "https://api.thingspeak.com/channels/1944585/field/1.json",
+  "https://api.thingspeak.com/channels/1944585/field/2.json",
+  "https://api.thingspeak.com/channels/1944585/field/3.json",
+  "https://api.thingspeak.com/channels/1944585/field/4.json",
+];
+
+const fetchData = async (url) => {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+};
+
+const fetchAllData = async () => {
+  const responses = await Promise.all(urls.map(url => fetchData(url)));
+  const data = responses.map(res => res.feeds[0].field1);
+
+  // Save the last value of field1 to the global variable
+//   lastValues1 = responses.map(res => res.feeds[res.feeds.length - 1].field1);
+//   lastValues2= responses.map(res => res.feeds[res.feeds.length - 1].field2);
+//   lastValues3= responses.map(res => res.feeds[res.feeds.length - 1].field3);
+// lastValues4 =responses.map(res => res.feeds[res.feeds.length - 1].field4);
+console.log(data)
+  return data;
+};
+
+const calculateAverage = (data) => {
+  const total = data.reduce((acc, val) => acc + Number(val), 0);
+  return total / data.length;
+};
+
+const main = async () => {
+  const data = await fetchAllData();
+  const average = calculateAverage(data);
+console.log(average)
+
+//   console.log(ans);
+}
+main();
